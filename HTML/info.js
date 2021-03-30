68482d473ce644fcddb8c05aa3a3709141f24dd4
GJKOP <66903641+MCGJKOP@users.noreply.github.com>
2021-03-30 13:20:53 +0800

    Create info.js

---
+++ HTML/info.js
@@ -0,0 +1,4 @@
+// <script src="info.js"></script>
+document.body.onload = function (){
+  console.info('%c'+'警告:\n使用这个控制台可能会导致网站失效，\n请不要随意粘贴别人给的代码,\n黑客会用XSS漏洞攻击网站','color:red;background:yellow;font-size:35px;');
+}
